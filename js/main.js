e2878d3c2b4ad9de0cd5830dc77ef21f897f52c9
Jason <jasonzhang666@163.com>
2022-02-23 22:39:32 +0800

    - 可以限制每行字数 - 支持设置字体大小，将来支持字体大小范围设置 - 支持设置图片容器大小

--- js/main.js
+++ js/main.js
@@ -1,29 +1,64 @@
-
 function textToImg() {
+
+    let rowWordNum = document.getElementById("word_num").value
+    if (rowWordNum < 10) {
+        rowWordNum = 10
+    }
+    if (rowWordNum > 40) {
+        rowWordNum = 40
+    }
+    let wordSize = document.getElementById("word_size").value
+    if (wordSize <= 0) {
+        wordSize = 10
+    }
+
+    let imgSizeX = document.getElementById("img_size_x").value
+    let imgSizeY = document.getElementById("img_size_y").value
+
 
     // 找到文本框内容
     let text = document.getElementById("txt_content").value
-    console.log(text)
     text = text.trim()
     let words = text.split("")
 
     // 找到 预转换的内容
     let toImgElement = document.getElementById("toimg")
     toImgElement.innerHTML = ""
+    toImgElement.removeAttribute("hidden")
+    // 設置圖片大小
+    toImgElement.setAttribute("style", "width: " + imgSizeX + "px !important;height: " + imgSizeY + "px !important;")
 
     //创建子节点
     let tb = document.createElement("table")
-    let tr = document.createElement("tr")
-    words.forEach(function (w, i) {
-        let td = document.createElement("td")
-        let innerDiv = document.createElement("div")
-        innerDiv.innerText = w
-        innerDiv.setAttribute("class", "icon-over")
-        innerDiv.setAttribute("style", "transform:rotate(" + random(0, 360) + "deg)")
-        td.appendChild(innerDiv)
-        tr.appendChild(td)
-    })
-    tb.appendChild(tr)
+
+    // 这么多字总共需要多少行
+    let rows = words.length / rowWordNum
+    let wordIndex;
+    for (let i = 0; i < rows; i++) {
+        let tr = document.createElement("tr")
+        for (let j = 0; j < rowWordNum; j++) {
+            wordIndex = i * rowWordNum + j
+            console.log(wordIndex, words[j])
+            let td = document.createElement("td")
+            let innerDiv = document.createElement("div")
+            innerDiv.innerText = words[wordIndex] ? words[wordIndex] : " "
+            // 字體旋轉
+            innerDiv.setAttribute("style", "transform:rotate(" + random(0, 360) + "deg);")
+            // 字體大小
+            tmpWordSize = parseInt(wordSize) + random(0, 0)
+            console.log(wordSize)
+            innerDiv.setAttribute("style", innerDiv.getAttribute("style") + "font-size: " + tmpWordSize + "px;")
+
+
+            td.appendChild(innerDiv)
+            tr.appendChild(td)
+        }
+        words.forEach(function (_, i) {
+
+        })
+        tb.appendChild(tr)
+    }
+
     toImgElement.appendChild(tb)
 
 
@@ -32,6 +67,8 @@ function textToImg() {
         let img = document.getElementById("img-base64");
         img.src = canvas.toDataURL("image/png");
     })
+    toImgElement.setAttribute("hidden", true)
+
 
 }
 
